7a297a1a082b1639067f5b9d29020bdc85592eb6
Maciek Panek <mpanek138@gmail.com>
2023-10-19 23:25:32 +0200

    Corrected grid design

--- src/pages/Villas.jsx
+++ src/pages/Villas.jsx
@@ -11,7 +11,7 @@ function Villas() {
   }
 
   return (
-    <section className="grid grid-cols-fluid gap-10  bg-stone-100 min-h-screen p-20">
+    <section className="grid grid-cols-fluid gap-10  bg-stone-100 min-h-screen auto-rows-max p-20">
       {villas.map((villa) => (
         <Villa villa={villa} key={villa.id} />
       ))}
