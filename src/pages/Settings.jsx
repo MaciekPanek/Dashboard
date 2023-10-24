35cde8e47593e592a7906f006aa757043d53c752
Maciek Panek <mpanek138@gmail.com>
2023-10-24 18:26:26 +0200

    fix

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,9 @@
 function Settings() {
-  return <section className=" bg-stone-100 h-screen ">Settings</section>;
+  return (
+    <section className=" bg-stone-100 h-screen ">
+      Settings fegbvertgvergfv
+    </section>
+  );
 }
 
 export default Settings;
