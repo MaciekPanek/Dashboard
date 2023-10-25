bb0ea779c050c9488dc2d60d2779afd01218d66e
Maciek Panek <mpanek138@gmail.com>
2023-10-25 12:25:42 +0200

    added active navbar item class

--- src/features/navigation/NavItem.jsx
+++ src/features/navigation/NavItem.jsx
@@ -2,8 +2,12 @@ import { NavLink } from "react-router-dom";
 
 function NavItem({ to, children }) {
   return (
-    <li className=" text-3xl text-neutral-500 mb-5  w-full ">
-      <NavLink className="flex flex-row gap-3" to={to}>
+    <li className=" text-3xl text-neutral-500   w-full  ">
+      <NavLink
+        className="flex flex-row gap-3 py-2 pl-5 "
+        to={to}
+        activeClassName="active"
+      >
         {children}
       </NavLink>
     </li>
