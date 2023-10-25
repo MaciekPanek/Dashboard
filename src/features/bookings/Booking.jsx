a9ee10e61dd3347215bfd566c2b5fb3ea82a8ead
Maciek Panek <mpanek138@gmail.com>
2023-10-25 19:46:59 +0200

    cleaned code

--- src/features/bookings/Booking.jsx
+++ src/features/bookings/Booking.jsx
@@ -15,15 +15,9 @@ function Booking({ booking }) {
   return (
     <div className="bg-stone-200">
       <div>
-        {/* <div>{name}</div> */}
         <div>{fullName}</div>
         <div>{arrivalDate}</div>
         <div>{departureDate}</div>
-        {/* <div>{guestNum}</div> */}
-        {/* <div>{id}</div> */}
-        {/* <div>{notes}</div> */}
-        {/* <div>{cost}</div> */}
-        {/* <div>{email}</div> */}
       </div>
     </div>
   );
