66fb9ae3a726b07c40648b04681710f97b3b8ca3
Maciek Panek <mpanek138@gmail.com>
2023-10-27 10:56:58 +0200

    fixed NavItem

--- src/features/navigation/NavItem.jsx
+++ src/features/navigation/NavItem.jsx
@@ -4,7 +4,7 @@ function NavItem({ to, children }) {
   return (
     <li className=" text-2xl text-neutral-500   w-full  ">
       <NavLink
-        className="flex flex-row gap-3 py-2 pl-5 "
+        className="flex flex-row gap-3 py-2 pl-5 items-center "
         to={to}
         activeclassname="active"
       >
